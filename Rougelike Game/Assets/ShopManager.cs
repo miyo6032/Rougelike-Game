2602e74117a35c1e83bbaab14e60c35242e1aed7
miyo6032 <miyo6032@colorado.edu>
2018-07-12 20:12:31 -0600

    Outline of shop accounting

---
+++ Rougelike Game/Assets/ShopManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour {
+
+    public int gold;
+
+    public void AddGold(int amount)
+    {
+        gold += amount;
+    }
+
+    /// <summary>
+    /// Will attempt to "buy" gold, and return a bool as to whether there is enough gold left
+    /// </summary>
+    public bool RemoveGold(int amount)
+    {
+        if(amount > gold)
+        {
+            return false;
+        }
+        gold -= amount;
+        return true;
+    }
+
+}
---
+++ Rougelike Game/Assets/ShopSlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShopSlot : ItemSlot {
+
+    public override void SetItem(ItemInstance itemInstance)
+    {
+        if(itemInstance == null)
+        {
+            Debug.Log("-gold");
+        }
+        base.SetItem(itemInstance);
+    }
+
+    public override void ItemDropIntoEmpty(ItemInstance droppedItem)
+    {
+        Debug.Log("+gold");
+        base.ItemDropIntoEmpty(droppedItem);
+    }
+
+    public override void ItemDropIntoFull(ItemInstance droppedItem)
+    {
+        Debug.Log("-gold" + item.item.Title);
+        Debug.Log("+gold" + droppedItem.item.Title);
+        base.ItemDropIntoFull(droppedItem);
+    }
+
+}
